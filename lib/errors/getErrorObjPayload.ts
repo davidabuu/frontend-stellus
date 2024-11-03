38358b4bfe8d3edd1c96d4d639606a095f5b39ac
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-03 20:45:12 +0100

    Update getErrorObjPayload.ts

--- lib/errors/getErrorObjPayload.ts
+++ lib/errors/getErrorObjPayload.ts
@@ -3,21 +3,15 @@ import getErrorObj from './getErrorObj';
 export default function getErrorObjPayload<Payload extends object>(error: unknown): Payload | undefined {
   const errorObj = getErrorObj(error);
 
-  if (!errorObj || !('payload' in errorObj)) {
-    return;
+  if (
+    errorObj &&
+    typeof errorObj === 'object' &&
+    'payload' in errorObj &&
+    typeof (errorObj as { payload: unknown }).payload === 'object' &&
+    !Array.isArray(errorObj.payload)
+  ) {
+    return errorObj.payload as Payload;
   }
 
-  if (typeof errorObj.payload !== 'object') {
-    return;
-  }
-
-  if (errorObj === null) {
-    return;
-  }
-
-  if (Array.isArray(errorObj)) {
-    return;
-  }
-
-  return errorObj.payload as Payload;
+  return undefined;
 }
