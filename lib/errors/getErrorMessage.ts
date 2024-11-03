647bfd8d671f139ccd44384534c4723e03eab3ee
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-03 20:39:24 +0100

    Update getErrorMessage.ts

--- lib/errors/getErrorMessage.ts
+++ lib/errors/getErrorMessage.ts
@@ -1,6 +1,15 @@
 import getErrorObj from './getErrorObj';
-
 export default function getErrorMessage(error: unknown): string | undefined {
   const errorObj = getErrorObj(error);
-  return errorObj && 'message' in errorObj && typeof errorObj.message === 'string' ? errorObj.message : undefined;
+  return isErrorWithMessage(errorObj) ? errorObj.message : undefined;
+}
+
+// Type guard to check if an object has a `message` property of type `string`
+function isErrorWithMessage(error: unknown): error is { message: string } {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as { message: unknown }).message === 'string'
+  );
 }
