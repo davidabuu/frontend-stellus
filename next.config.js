2ae3daf2451c5e626e943d07648108ac9b7dc83f
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-03 19:46:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,17 +6,7 @@ const withRoutes = require('nextjs-routes/config')({
   outDir: 'nextjs',
 });
 
-const headers = require('./nextjs/headers');
-const redirects = require('./nextjs/redirects');
-const rewrites = require('./nextjs/rewrites');
-
-/** @type {import('next').NextConfig} */
 const moduleExports = {
-  transpilePackages: [
-    'react-syntax-highlighter',
-    'swagger-client',
-    'swagger-ui-react',
-  ],
   reactStrictMode: true,
   webpack(config, { webpack }) {
     config.plugins.push(
@@ -25,37 +15,45 @@ const moduleExports = {
         __SENTRY_TRACING__: false,
       }),
     );
-    config.module.rules.push(
-      {
-        test: /\.svg$/,
-        use: [ '@svgr/webpack' ],
-      },
-    );
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
 
     return config;
   },
-  // NOTE: all config functions should be static and not depend on any environment variables
-  // since all variables will be passed to the app only at runtime and there is now way to change Next.js config at this time
-  // if you are stuck and strongly believe what you need some sort of flexibility here please fill free to join the discussion
-  // https://github.com/blockscout/frontend/discussions/167
-  rewrites,
-  redirects,
-  headers,
+
   output: 'standalone',
   productionBrowserSourceMaps: true,
-  experimental: {
-    instrumentationHook: process.env.NEXT_OPEN_TELEMETRY_ENABLED === 'true',
-    // disabled as it is not stable yet
-    // turbo: {
-    //   rules: {
-    //     '*.svg': {
-    //       loaders: [ '@svgr/webpack' ],
-    //       as: '*.js',
-    //     },
-    //   },
-    // },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "connect-src 'self' https://wss:// raw.githubusercontent.com",
+              "coinzilla.com *.coinzilla.com https://request-global.czilladx.com",
+              "servedbyadbutler.com *.slise.xyz api.hypelab.com",
+              "*.ixncdn.com *.cloudfront.net v1.getittech.io ipapi.co",
+              "fonts.gstatic.com sentry.io *.sentry.io;",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' *.undefined ... ;",
+              "style-src 'self' 'unsafe-inline' data: fonts.googleapis.com ...; ...",
+            ].join(' '),
+          },
+        ],
+      },
+    ];
+  },
+
+  // Disable ESLint during build
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
