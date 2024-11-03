607c188d492d656759ff2b9c6a4bcb992b19d660
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-03 20:27:00 +0100

    Update getErrorCause.ts

--- lib/errors/getErrorCause.ts
+++ lib/errors/getErrorCause.ts
@@ -1,7 +1,7 @@
 export default function getErrorCause(error: Error | undefined): Record<string, unknown> | undefined {
   return (
     error && 'cause' in error &&
-    typeof error.cause === 'object' && error.cause !== null
-  ) ||
-    undefined;
+    typeof error.cause === 'object' &&
+    error.cause !== null
+  ) ? error.cause as Record<string, unknown> : undefined;
 }
