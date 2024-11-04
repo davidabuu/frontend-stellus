3309149dd7ade51fd39778b96dd35fadd9547b4d
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-04 10:13:42 +0100

    Update throwOnAbsentParamError.ts

--- lib/errors/throwOnAbsentParamError.ts
+++ lib/errors/throwOnAbsentParamError.ts
@@ -1,5 +1,16 @@
+// Custom error class that includes a status property
+class CustomError extends Error {
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+    this.name = 'CustomError';
+  }
+}
+
 export default function throwOnAbsentParamError(param: unknown) {
   if (!param) {
-    throw new Error('Required param not provided', { cause: { status: 404 } });
+    throw new CustomError('Required param not provided', 404);
   }
 }
