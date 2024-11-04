e1f26136a6df522401d2d12cd53fc5255a690878
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-04 10:38:46 +0100

    Update schema.ts

--- lib/solidityScan/schema.ts
+++ lib/solidityScan/schema.ts
@@ -1,27 +1,27 @@
 import * as v from 'valibot';
 
 // Define the schema for issue severity distribution
-export const SolidityScanIssueSeverityDistributionSchema = v.schema({
+export const SolidityScanIssueSeverityDistributionSchema = {
   critical: v.number(),
   gas: v.number(),
   high: v.number(),
   informational: v.number(),
   low: v.number(),
   medium: v.number(),
-});
+};
 
 // Define the main schema for Solidity scan report
-export const SolidityScanSchema = v.schema({
-  scan_report: v.schema({
+export const SolidityScanSchema = {
+  scan_report: {
     contractname: v.string(),
     scan_status: v.string(),
-    scan_summary: v.schema({
+    scan_summary: {
       score_v2: v.string(),
       issue_severity_distribution: SolidityScanIssueSeverityDistributionSchema,
-    }),
+    },
     scanner_reference_url: v.string(),
-  }),
-});
+  },
+};
 
 // Define types for the inferred outputs
 export type SolidityScanReport = v.InferOutput<typeof SolidityScanSchema>;
