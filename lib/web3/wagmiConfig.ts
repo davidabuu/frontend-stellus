62d2acc425e6aef6b571bfa5f34a420cd22b88f2
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-04 14:26:38 +0100

    Update wagmiConfig.ts

--- lib/web3/wagmiConfig.ts
+++ lib/web3/wagmiConfig.ts
@@ -1,38 +1,45 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { http } from 'viem';
-import { type CreateConfigParameters } from 'wagmi';
-
 import config from 'configs/app';
 import currentChain from 'lib/web3/currentChain';
+
 const feature = config.features.blockchainInteraction;
 
 const wagmiConfig = (() => {
-  // Define chains as a tuple with at least one Chain item
-  const chains: [typeof currentChain] = [currentChain];
+  const chains = [currentChain];
 
-  return defaultWagmiConfig({
+  const commonConfig = {
     chains,
-    multiInjectedProviderDiscovery: feature.isEnabled,
     transports: {
-      [currentChain.id]: http(feature.isEnabled ? undefined : config.chain.rpcUrl || `${config.api.endpoint}/api/eth-rpc`),
+      [currentChain.id]: http(
+        feature.isEnabled ? '' : config.chain.rpcUrl || `${config.api.endpoint}/api/eth-rpc`
+      ),
     },
-    projectId: feature.isEnabled ? feature.walletConnect.projectId : undefined,
-    metadata: feature.isEnabled
-      ? {
-          name: `${config.chain.name} explorer`,
-          description: `${config.chain.name} explorer`,
-          url: config.app.baseUrl,
-          icons: [config.UI.navigation.icon.default].filter(Boolean),
-        }
-      : undefined,
-    auth: feature.isEnabled
-      ? {
-          email: true,
-          socials: [],
-        }
-      : undefined,
     ssr: true,
     batch: { multicall: { wait: 100 } },
+  };
+
+  if (!feature.isEnabled) {
+    return defaultWagmiConfig({
+      ...commonConfig,
+      // Additional non-enabled config, if needed
+    });
+  }
+
+  return defaultWagmiConfig({
+    ...commonConfig,
+    multiInjectedProviderDiscovery: true,
+    projectId: feature.walletConnect.projectId!, // Use `!` to assert it's defined when `feature.isEnabled` is `true`
+    metadata: {
+      name: `${config.chain.name} explorer`,
+      description: `${config.chain.name} explorer`,
+      url: config.app.baseUrl,
+      icons: [config.UI.navigation.icon.default].filter(Boolean),
+    },
+    auth: {
+      email: true,
+      socials: [],
+    },
   });
 })();
 
