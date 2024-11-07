634f0aa41a6794f68ee5936f95ed8dc67a4e9d7b
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-07 23:34:04 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           { /* FONTS */ }
+          <title>Stellus Explorer</title>
           <link
             href={ config.UI.fonts.heading?.url ?? 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap' }
             rel="stylesheet"
@@ -48,12 +49,12 @@ class MyDocument extends Document {
           <script src="/assets/envs.js"/>
 
           { /* FAVICON */ }
-          <link rel="icon" href="/assets/favicon/favicon.ico" sizes="48x48"/>
-          <link rel="icon" sizes="32x32" type="image/png" href="/assets/favicon/favicon-32x32.png"/>
-          <link rel="icon" sizes="16x16" type="image/png"href="/assets/favicon/favicon-16x16.png"/>
-          <link rel="apple-touch-icon" href="/assets/favicon/apple-touch-icon-180x180.png"/>
-          <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg"/>
-
+          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="48x48" href="/assets/favicon/favicon-48x48.png"/>
+          <link rel="shortcut icon" href="/assets/favicon/favicon.ico"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon-180x180.png"/>
+          <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/android-chrome-192x192.png"/>
           <link rel="preload" as="image" href={ svgSprite.href }/>
         </Head>
         <body>
