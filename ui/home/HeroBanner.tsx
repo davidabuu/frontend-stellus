cb30b7d50da1ff7ea725b57bb7d6e67f3480496a
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2024-11-08 10:45:23 +0100

    Update HeroBanner.tsx

--- ui/home/HeroBanner.tsx
+++ ui/home/HeroBanner.tsx
@@ -2,7 +2,6 @@ import { Box, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 
 import config from 'configs/app';
-import AdBanner from 'ui/shared/ad/AdBanner';
 import SearchBar from 'ui/snippets/searchBar/SearchBar';
 import UserProfileDesktop from 'ui/snippets/user/profile/UserProfileDesktop';
 import UserWalletDesktop from 'ui/snippets/user/wallet/UserWalletDesktop';
@@ -77,7 +76,6 @@ const HeroBanner = () => {
         </Flex>
         <SearchBar isHomepage/>
       </Box>
-      <AdBanner platform="mobile" w="fit-content" flexShrink={ 0 } borderRadius="md" overflow="hidden" display={{ base: 'none', lg: 'block ' }}/>
     </Flex>
   );
 };
